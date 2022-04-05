881b938238ee63d23db4043213fb1ff8e63da376
Soham Chowdhury <77236783+code-soham@users.noreply.github.com>
2022-04-05 16:20:05 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+require("./db/dbconfig");
+var cors = require("cors");
+const app = express();
+const User = require("./models/user");
+// const Id =require("./models/mock");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("BloodMates Backend!");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const newUser = new User(req.body);
+  newUser.save((err) => {
+    if (err) {
+      res.status(500).json({ msg: "Sorry, internal error" });
+      console.log(err)
+    } else {
+      res.json({
+        msg: "data saved",
+      });
+    }
+  });
+});
+app.listen(process.env.PORT || 8000, async () => {
+  console.log(`Listening at port ${process.env.PORT || 8000}`);
+});
---
+++ backend/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+    // unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    // unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  bloodGroup: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  loc: { type: { type: String }, coordinates: [Number] },
+});
+userSchema.index({ loc: "2dsphere" });
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
